221f92c5623aef7c24fea2c556402e9e32b9e0c2
leandro0404 <leandrobhmgg@gmail.com>
2023-04-13 18:39:38 -0300

    init repo api go lang

--- controllers/home_controller.go
+++ controllers/home_controller.go
@@ -8,11 +8,11 @@ import (
 
 func HomeHandler(rw http.ResponseWriter, r *http.Request) {
 
-	pessoa := models.Person{Name: "Fulano", Age: 30}
+	person := models.Person{Name: "Fulano", Age: 30}
 	rw.Header().Set("Content-Type", "application/json")
 	rw.WriteHeader(http.StatusOK)
 
-	err := json.NewEncoder(rw).Encode(pessoa)
+	err := json.NewEncoder(rw).Encode(person)
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 	}
